a691d1e61fda0be29d82b88b7bda9c75b96c39b5
MatamaPeter <peter24matama@gmail.com>
2025-05-03 17:48:36 +0300

    refactor: Remove unused font family configuration from Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,7 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,vue}"],
   theme: {
     extend: {
-      fontFamily: {
-        'inter': ['Inter', 'sans-serif'],
-        'playfair': ['"Playfair Display"', 'serif'],
-      },
+      
     },
   },
   plugins: [],
