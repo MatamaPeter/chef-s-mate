58878bf1730a1831a7fde4017a63ecf3fc82ba24
MatamaPeter <peter24matama@gmail.com>
2025-05-03 18:18:58 +0300

    refactor: Add color definitions for amber in Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,15 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,vue}"],
   theme: {
     extend: {
-      
-    },
-  },
-  plugins: [],
+      colors: {
+        amber: {
+          400: '#fbbf24',
+          500: '#f59e0b',
+          600: '#d97706'
+        }
+      }
+    }
+  }
+ 
 }
+
